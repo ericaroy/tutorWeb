1eb651fa8d82b4a6e26321e84e2e24ff8e4a00eb
Erica Roy <digitalnico@icloud.com>
2017-10-23 13:47:05 -0500

    Changed secret

--- app/auth.py
+++ app/auth.py
@@ -8,7 +8,13 @@ config = {
   "authDomain": "campustutors-78ccb.firebaseapp.com",
   "databaseURL": "https://campustutors-78ccb.firebaseio.com",
   "storageBucket": "campustutors-78ccb.appspot.com",
-  "serviceAccount": "app/secret.json"
+  "serviceAccount": {
+        'client_email': os.environ['FIREBASE_CLIENT_EMAIL'],
+        'client_id': os.environ['FIREBASE_CLIENT_ID'],
+        'private_key': os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n', '\n'),
+        'private_key_id': os.environ['FIREBASE_PRIVATE_KEY_ID'],
+        'type': 'service_account'
+    }
 }
 
 
