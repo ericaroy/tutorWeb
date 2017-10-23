0cbc6e9db2a90f20804bbdb15d1cd8e69067695b
Erica Roy <digitalnico@icloud.com>
2017-10-23 14:10:31 -0500

    added secret for demo

--- app/auth.py
+++ app/auth.py
@@ -8,13 +8,7 @@ config = {
   "authDomain": "campustutors-78ccb.firebaseapp.com",
   "databaseURL": "https://campustutors-78ccb.firebaseio.com",
   "storageBucket": "campustutors-78ccb.appspot.com",
-  "serviceAccount": {
-        'client_email': os.environ['FIREBASE_CLIENT_EMAIL'],
-        'client_id': os.environ['FIREBASE_CLIENT_ID'],
-        'private_key': os.environ['FIREBASE_PRIVATE_KEY'].replace('\\n', '\n'),
-        'private_key_id': os.environ['FIREBASE_PRIVATE_KEY_ID'],
-        'type': 'service_account'
-    }
+  "serviceAccount": "app/secret.json"
 }
 
 
