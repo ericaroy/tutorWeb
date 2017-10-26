90a1d293e193fa87a7e8c822c470af18f67fefcc
Erica Roy <digitalnico@icloud.com>
2017-10-26 16:47:07 -0500

    Added prem authentication

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,39 @@ import os
 from app.userlogic import grab_all_tutors
 from flask_login import LoginManager
 from app.forms.base_forms import TutorForm, LoginForm, RegistrationForm
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session, json
 from flask_admin import Admin
+from flask_oauthlib.client import OAuth
 
 app = Flask(__name__)
 admin = Admin(app, name='Tutor App', template_mode='bootstrap3')
 the_login_manager = LoginManager()
 the_login_manager.init_app(app)
+oauth = OAuth(app)
+app.secret_key = os.environ['SECRET_KEY']
 
+google = oauth.remote_app(
+    'google',
+    consumer_key=os.environ['GOOGLE_CLIENT_ID'],
+    consumer_secret=os.environ['GOOGLE_CLIENT_SECRET'],
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
 
 @app.route('/')
 def index():
+	if 'google_token' in session:
+		me = google.get('userinfo')
+		x = json.dumps(me.data)
+		f = json.loads(x)
+
+		return render_template('index.html', f=f)
 	return render_template('index.html')
 
 
@@ -21,6 +43,15 @@ def find_tutors():
 
 	tutors = grab_all_tutors()
 	return render_template('findtutors.html', tutors=tutors)
+
+
+@app.route('/profile')
+def get_profile():
+	if 'google_token' in session:
+		me = google.get('userinfo')
+		x = json.dumps(me.data)
+		f = json.loads(x)
+	return render_template('profile.html',f=f)
 
 
 @app.route('/tutorapp', methods=['GET', 'POST'])
@@ -33,9 +64,38 @@ def tutorapp():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm(request.form, csrf_enabled=False)
+	if request.method == 'GET':
 
-	return render_template('login.html', form=form)
+		return google.authorize(callback=url_for('authorized', _external=True))
+
+
+	#Check Firebase to see if there is already an account before creating, if so pass
+	return render_template('index')
+
+
+@app.route('/logout')
+def logout():
+	session.pop('google_token', None)
+	return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = google.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['google_token'] = (resp['access_token'], '')
+    me = google.get('userinfo')
+
+    return redirect(url_for('index', me=me))
+
+
+@google.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
 
 
 @app.route('/register', methods=['GET', 'POST'])
